0cc6811ca649a22f7cc7573c79e20427d9ef8340
Saurabh Dhandare <saurabhdhandare@gmail.com>
2021-03-07 23:24:09 -0800

    Add files via upload
    First program.

---
+++ Person.java
@@ -0,0 +1,27 @@
+public class Person{
+	int age;
+	String name;
+
+	public Person(int a, String n) {
+		age = a;
+		name = n;
+	}
+
+	public void drive(){
+		if  (age < 18) {
+			System.out.println("Can Drive");
+		}
+		else {
+			System.out.println("Learn Driving");
+		}
+	}
+	
+	//main method
+	public static void main(String[] args){
+		Person p1 = new Person(18, "shubhangi");
+		Person p2 = new Person(16, "sanju");
+
+		p1.drive();
+		p2.drive();
+	}
+}
